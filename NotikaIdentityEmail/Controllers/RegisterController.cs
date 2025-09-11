aff219731ffbefbb7ea262c414168d29850281e2
tubabagcidogan <tubabagcidogan@gmail.com>
2025-09-11 16:38:23 +0300

    Add project files.

---
+++ NotikaIdentityEmail/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(RegisterUserViewModel model)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name = model.Name,
+                Email = model.Email,
+                Surname = model.Surname,
+                UserName = model.Username
+            };
+
+            var result = await _userManager.CreateAsync(appUser, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("UserLogin", "Login");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+
+                return View();
+            }
+        }
+    }
+}
