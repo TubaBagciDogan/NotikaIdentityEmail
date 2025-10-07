9f0c8b873b70a6b84b256612b2a2dfe47345be14
tubabagcidogan <tubabagcidogan@gmail.com>
2025-10-07 16:40:01 +0300

    rol

---
+++ NotikaIdentityEmail/Controllers/RoleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
+using NotikaIdentityEmail.Models;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            IdentityRole role = new IdentityRole()
+            {
+                Name = model.RoleName
+            };
+            await _roleManager.CreateAsync(role);
+            return RedirectToAction("RoleList");
+        }
+    }
+}
