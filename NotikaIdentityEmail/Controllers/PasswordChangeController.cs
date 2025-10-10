d0c03cfdd66a30cfd77d42dccc53f27554b54097
tubabagcidogan <tubabagcidogan@gmail.com>
2025-10-10 11:41:22 +0300

    hata sayfaları

---
+++ NotikaIdentityEmail/Controllers/PasswordChangeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class PasswordChangeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
