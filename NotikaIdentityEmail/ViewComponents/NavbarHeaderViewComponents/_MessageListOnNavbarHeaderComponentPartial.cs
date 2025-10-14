96f1980d6ed6cda675782d983b908cd3fcbbf94d
tubabagcidogan <tubabagcidogan@gmail.com>
2025-10-14 10:13:28 +0300

    ...

---
+++ NotikaIdentityEmail/ViewComponents/NavbarHeaderViewComponents/_MessageListOnNavbarHeaderComponentPartial.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Context;
+using NotikaIdentityEmail.Entities;
+
+namespace NotikaIdentityEmail.ViewComponents.NavbarHeaderViewComponents
+{
+    public class _MessageListOnNavbarHeaderComponentPartial : ViewComponent
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly EmailContext _context;
+
+        public _MessageListOnNavbarHeaderComponentPartial(UserManager<AppUser> userManager, EmailContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var values = _context.Messages.Where(x => x.ReceiverEmail == user.Email && x.IsRead == false).ToList();
+            return View(values);
+        }
+    }
+}
